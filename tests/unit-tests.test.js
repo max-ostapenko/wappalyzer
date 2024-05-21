a8f9ffe04b424ef60fa34290b5ec4e4e3ba9445b
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-05-21 22:36:46 +0200

    corrected test condition

--- tests/unit-tests.test.js
+++ tests/unit-tests.test.js
@@ -5,8 +5,10 @@ const testWebsite = 'https://almanac.httparchive.org/en/2022/'
 let responseData
 beforeAll(async () => {
   responseData = await runWPTTest(testWebsite)
+  firstView = responseData.data.runs['1'].firstView
 }, 400000)
 
 test('wappalyzer successful', () => {
-  assert.ok(responseData.runs['1'].firstView.wappalyzer_failed === 0)
+  assert(firstView.wappalyzer_failed === undefined, 'wappalyzer_failed key is present')
+  assert(Object.keys(firstView.detected_technologies).length > 1, 'number of technologies detected <=1')
 })
