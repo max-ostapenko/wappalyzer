9a747dde856790ab32b54723a743ead10b786c9e
Max Ostapenko <1611259+max-ostapenko@users.noreply.github.com>
2024-05-21 22:40:53 +0200

    lint

--- tests/unit-tests.test.js
+++ tests/unit-tests.test.js
@@ -2,13 +2,19 @@ const assert = require('assert')
 const { runWPTTest } = require('./wpt.js')
 const testWebsite = 'https://almanac.httparchive.org/en/2022/'
 
-let responseData
+let responseData, firstView
 beforeAll(async () => {
   responseData = await runWPTTest(testWebsite)
   firstView = responseData.data.runs['1'].firstView
 }, 400000)
 
 test('wappalyzer successful', () => {
-  assert(firstView.wappalyzer_failed === undefined, 'wappalyzer_failed key is present')
-  assert(Object.keys(firstView.detected_technologies).length > 1, 'number of technologies detected <=1')
+  assert(
+    firstView.wappalyzer_failed === undefined,
+    'wappalyzer_failed key is present'
+  )
+  assert(
+    Object.keys(firstView.detected_technologies).length > 1,
+    'number of technologies detected <=1'
+  )
 })
